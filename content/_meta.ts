90bfd9137d4c4a05ff78b3f567cd0b755e382cf8
Joe Hart <joe@jeverest.com>
2025-12-08 11:26:58 -0800

    Add Docusaurus documentation site

---
+++ content/_meta.ts
@@ -0,0 +1,15 @@
+export default {
+  'index': 'Home',
+  'adjusting-the-database-collation': 'Adjusting the Database Collation',
+  'setup-with-docker': 'Setup with Docker',
+  'third-party-tools-and-libraries': 'Third Party Tools and Libraries',
+  'troubleshooting': 'Troubleshooting',
+  'v2-changelog': 'v2 Changelog',
+  'linkace-translations': 'LinkAce Translations',
+  'likeace-overview': 'LikeAce Overview',
+  'api-linkace-openapi': {
+    title: 'Linkace Openapi',
+    type: 'page',
+    href: '/api-docs-linkace-openapi.html'
+  }
+}
