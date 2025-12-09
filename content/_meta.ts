41d0d0a73c37e2e78c0f0eda4670bdf7d114c71a
Joe Hart <joe@jeverest.com>
2025-12-08 16:06:52 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,15 +1,33 @@
 export default {
-  'index': 'Home',
-  'setup-with-docker': 'Setup with Docker',
-  'setup-with-php': 'Setup with PHP',
-  'third-party-tools-and-libraries': 'Third Party Tools and Libraries',
-  'troubleshooting': 'Troubleshooting',
-  'v2-changelog': 'v2 Changelog',
-  'linkace-translations': 'LinkAce Translations',
-  'likeace-overview': 'LikeAce Overview',
-  'api-linkace-openapi': {
-    title: 'Linkace Openapi',
-    type: 'page',
-    href: '/api-docs-linkace-openapi.html'
+  "index": {
+    "title": "Getting Started",
+    "type": "separator"
+  },
+  "setup-with-docker": "Setup with Docker",
+  "setup-with-php": "Setup with PHP",
+
+  "guides": {
+    "title": "Guides",
+    "type": "separator"
+  },
+  "third-party-tools-and-libraries": "Third Party Tools and Libraries",
+  "troubleshooting": "Troubleshooting",
+
+  "reference": {
+    "title": "Reference",
+    "type": "separator"
+  },
+  "v2-changelog": "v2 Changelog",
+  "linkace-translations": "LinkAce Translations",
+  "likeace-overview": "LikeAce Overview",
+
+  "api": {
+    "title": "API Documentation",
+    "type": "separator"
+  },
+  "api-linkace-openapi": {
+    "title": "LinkAce OpenAPI",
+    "type": "page",
+    "href": "/api-docs-linkace-openapi.html"
   }
 }
