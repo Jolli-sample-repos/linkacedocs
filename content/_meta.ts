6467809dde9613a65b9464021fe7707029094574
Joe Hart <joe@jeverest.com>
2025-12-08 16:07:22 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,33 +1,27 @@
 export default {
-  "index": {
-    "title": "Getting Started",
-    "type": "separator"
+  'index': 'Home',
+  'setup-with-docker': 'Setup with Docker',
+  'setup-with-php': 'Setup with PHP',
+  'guides': {
+    type: 'separator',
+    title: 'Guides'
   },
-  "setup-with-docker": "Setup with Docker",
-  "setup-with-php": "Setup with PHP",
-
-  "guides": {
-    "title": "Guides",
-    "type": "separator"
+  'third-party-tools-and-libraries': 'Third Party Tools and Libraries',
+  'troubleshooting': 'Troubleshooting',
+  'reference': {
+    type: 'separator',
+    title: 'Reference'
   },
-  "third-party-tools-and-libraries": "Third Party Tools and Libraries",
-  "troubleshooting": "Troubleshooting",
-
-  "reference": {
-    "title": "Reference",
-    "type": "separator"
+  'v2-changelog': 'v2 Changelog',
+  'linkace-translations': 'LinkAce Translations',
+  'likeace-overview': 'LikeAce Overview',
+  'api': {
+    type: 'separator',
+    title: 'API Documentation'
   },
-  "v2-changelog": "v2 Changelog",
-  "linkace-translations": "LinkAce Translations",
-  "likeace-overview": "LikeAce Overview",
-
-  "api": {
-    "title": "API Documentation",
-    "type": "separator"
-  },
-  "api-linkace-openapi": {
-    "title": "LinkAce OpenAPI",
-    "type": "page",
-    "href": "/api-docs-linkace-openapi.html"
+  'api-linkace-openapi': {
+    title: 'Linkace Openapi',
+    type: 'page',
+    href: '/api-docs-linkace-openapi.html'
   }
 }
